69526180cc0bbbc3b992f9415ecf08dad4683911
Marko Kofol <mk8394@student.uni-lj.si>
2022-04-02 17:57:42 +0200

    Separate register activity.

---
+++ app/src/main/java/si/uni_lj/fe/tnuv/dezurka/RegisterActivity.java
@@ -0,0 +1,88 @@
+package si.uni_lj.fe.tnuv.dezurka;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private Button registerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        registerBtn = findViewById(R.id.register_btn);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        registerBtn.setOnClickListener(v -> {
+            EditText username = findViewById(R.id.username_input);
+            EditText email = findViewById(R.id.email_input);
+            EditText password = findViewById(R.id.password_input);
+            EditText passwordConfirm = findViewById(R.id.confirm_password_input);
+            if (username == null || username.length() == 0 ||
+                email == null || email.length() == 0 ||
+                password == null || password.length() == 0 ||
+                passwordConfirm == null || passwordConfirm.length() == 0
+            ) return;
+            createAccount(
+                    username.getText().toString(),
+                    email.getText().toString(),
+                    password.getText().toString(),
+                    passwordConfirm.getText().toString()
+            );
+        });
+    }
+
+    public void createAccount(String username, String email, String password, String passwordConfirm) {
+        if (!password.equals(passwordConfirm)) {
+            Toast.makeText(RegisterActivity.this,
+                    getResources().getString(R.string.passwords_do_not_match_msg),
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(username)
+                                //.setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
+                                .build();
+
+                        user.updateProfile(profileUpdates)
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        //updateUI(user);
+                                        Toast.makeText(RegisterActivity.this,
+                                                getResources().getString(R.string.register_successful_msg),
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(RegisterActivity.this, task.getException().getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                        //updateUI(null);
+                    }
+                });
+    }
+}
--- app/src/main/java/si/uni_lj/fe/tnuv/dezurka/SignInActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/dezurka/SignInActivity.java
@@ -1,24 +1,17 @@
 package si.uni_lj.fe.tnuv.dezurka;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
@@ -43,19 +36,14 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             //reload();
-            Toast.makeText(LoginActivity.this,
+            Toast.makeText(SignInActivity.this,
                     getResources().getString(R.string.signIn_successful_msg) + currentUser.getEmail(),
                     Toast.LENGTH_SHORT).show();
         }
 
         registerBtn.setOnClickListener(v -> {
-            EditText email = findViewById(R.id.email_input);
-            EditText password = findViewById(R.id.password_input);
-            if (email == null || email.length() == 0 || password == null || password.length() == 0) return;
-            createAccount(
-                    email.getText().toString(),
-                    password.getText().toString()
-            );
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
         });
         loginBtn.setOnClickListener(v -> {
             EditText email = findViewById(R.id.email_input);
@@ -68,34 +56,17 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void createAccount(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        //updateUI(user);
-                        Toast.makeText(LoginActivity.this,
-                                getResources().getString(R.string.register_successful_msg),
-                                Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(LoginActivity.this, task.getException().getMessage(),
-                                Toast.LENGTH_SHORT).show();
-                        //updateUI(null);
-                    }
-                });
-    }
-
     public void signIn(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         FirebaseUser user = mAuth.getCurrentUser();
                         // updateUI(user);
-                        Toast.makeText(LoginActivity.this,
+                        Toast.makeText(SignInActivity.this,
                                 getResources().getString(R.string.signIn_successful_msg) + user.getEmail(),
                                 Toast.LENGTH_SHORT).show();
                     } else {
-                        Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                        Toast.makeText(SignInActivity.this, task.getException().getMessage(),
                                 Toast.LENGTH_SHORT).show();
                         //updateUI(null);
                     }
