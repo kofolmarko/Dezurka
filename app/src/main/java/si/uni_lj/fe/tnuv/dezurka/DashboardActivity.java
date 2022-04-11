e324782ebd93f2b76da11e6083c28685a9a0dee2
Marko Kofol <mk8394@student.uni-lj.si>
2022-04-11 09:12:38 +0200

    Dashboard Activity as Empty Activity; font integration.

--- app/src/main/java/si/uni_lj/fe/tnuv/dezurka/DashboardActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/dezurka/DashboardActivity.java
@@ -1,46 +1,14 @@
 package si.uni_lj.fe.tnuv.dezurka;
 
-import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import si.uni_lj.fe.tnuv.dezurka.databinding.ActivityDashboardBinding;
+
+import android.os.Bundle;
 
 public class DashboardActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-     binding = ActivityDashboardBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dashboard);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.hamburgerMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dashboard);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        setContentView(R.layout.activity_dashboard);
     }
 }
