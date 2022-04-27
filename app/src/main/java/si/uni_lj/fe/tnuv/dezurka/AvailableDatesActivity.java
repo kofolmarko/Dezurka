bfc1fdb4a5812f56810a0611a9344381dd08887c
Marko Kofol <mk8394@student.uni-lj.si>
2022-04-27 14:36:26 +0200

    Added new activities and constants.

---
+++ app/src/main/java/si/uni_lj/fe/tnuv/dezurka/AvailableDatesActivity.java
@@ -0,0 +1,14 @@
+package si.uni_lj.fe.tnuv.dezurka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailableDatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_dates);
+    }
+}
---
+++ app/src/main/java/si/uni_lj/fe/tnuv/dezurka/TradesActivity.java
@@ -0,0 +1,13 @@
+package si.uni_lj.fe.tnuv.dezurka;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TradesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trades);
+    }
+}
